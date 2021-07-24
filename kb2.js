134fc74124bbbda2ae980a1ea100fb7a83c30a94
manav9556 <54392955+manav9556@users.noreply.github.com>
2021-07-25 02:37:16 +0530

    new

---
+++ kb2.js
@@ -0,0 +1,70 @@
+var numberOfDrumButtons = document.querySelectorAll(".b1").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".b1")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    
+
+  });
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+
+
+});
+function makeSound(key) {
+
+  switch (key) {
+    case "h":
+      var tom1 = new Audio("New folder/tum jiyo hazaro saal .mp3");
+      tom1.play();
+      break;
+
+    case "b":
+        document.getElementsByClassName("h").disabled = true
+      var tom2 = new Audio("New folder/Tum Mile - Tum Mile1.mp3");
+      tom2.play();
+      break;
+
+    case "d":
+        document.getElementsByClassName("b").disabled = true
+      var tom3 = new Audio('New folder/Tera Birthday Aaya Hai 1.mp3');
+      tom3.play();
+      break;
+
+    
+    default: console.log(key);
+
+  }
+}
+
+// var bt1=document.querySelector(".b1").addEventListener("click", function(){
+//   var buttonInnerHTML = this.innerHTML;
+// makeSound1(buttonInnerHTML);
+// pauseVid(buttonInnerHTML)
+// });
+// function makeSound1()
+// {
+//   var audio = new Audio("New folder/tum jiyo hazaro saal .mp3");
+// audio.play(); 
+
+// }
+// document.querySelector(".b2").addEventListener("click", function(){
+//   var buttonInnerHTML2 = this.innerHTML;
+// makeSound(buttonInnerHTML2);
+//  makeSound1()==false
+
+// });
+// function makeSound()
+// {
+//   var audio = new Audio("New folder/Tum Mile - Tum Mile 128 Kbps.mp3");
+// audio.play(); 
+}
+// 
